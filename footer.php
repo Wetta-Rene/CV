55667a4f6dc9c3c0a3f13aa602a42291a9ba9291
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-04-16 22:00:57 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-	<footer>
-        <p>Suivez moi sur:</p>
-			<p id="pLogoLinkedin"><a href="https://www.linkedin.com/in/rene-wetta" target="_blank"><img src="CSS/IMAGES/LinkedIn_logo_initials.png" /></a></p>
-			<p id="pLogoViadeo"><a href="https://www.viadeo.com/p/002ailrfea4zsow" target="_blank"><img src="CSS/IMAGES/LogoViadeo.png" /></a></p>	
-
-	</footer>
- 
-</body>
-</html>
